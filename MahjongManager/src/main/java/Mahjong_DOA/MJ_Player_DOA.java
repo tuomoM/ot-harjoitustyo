d92da4033643ed7fd194f942be45bbfa6262ff53
TuomoM <tuomo.mehtala@gmail.com>
2020-11-17 21:56:04 +0200

    Mahjong program classes, under construction

---
+++ MahjongManager/src/main/java/Mahjong_DOA/MJ_Player_DOA.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mahjong_DOA;
+
+/**
+ *
+ * @author tuomomehtala
+ */
+public class MJ_Player_DOA {
+    
+}
